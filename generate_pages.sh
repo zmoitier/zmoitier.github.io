5fe68b0053ba9ae0033ba76d2039fb9b88af0dde
Zoïs Moitier <zmoitier@ucmerced.edu>
2022-11-02 23:25:17 +0100

    change static to docs because github

--- generate_pages.sh
+++ generate_pages.sh
@@ -5,6 +5,7 @@
 #     pandoc --from html --to markdown "$FILE" --output "pages/$BASENAME.md"
 # done
 
-pandoc --from markdown_strict --to html pages/presentation.md --output static/presentation.html
-pandoc --from markdown_strict --to html pages/enseignement.md --output static/enseignement.html
-pandoc --from markdown_strict --to html pages/research.md --output static/research.html
+pandoc --from markdown_strict --to html pages/presentation.md --output docs/presentation.html
+pandoc --from markdown_strict --to html pages/enseignement.md --output docs/enseignement.html
+pandoc --from markdown_strict --to html pages/research.md --output docs/research.html
+
